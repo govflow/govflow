3677452f20066a4ab6f8c63ed51b00ba076b533a
Paul Walsh <paul@walsh.co.il>
2021-10-24 14:29:29 +0300

    Basic xml support for open311.

--- src/core/open311/routes.ts
+++ src/core/open311/routes.ts
@@ -1,13 +1,13 @@
 import { Request, Response, Router } from 'express';
+import xmlparser from 'express-xml-bodyparser';
+import xml2js from 'xml2js';
 import { wrapAsync } from '../../helpers';
 import { serviceAs311 } from '../services/helpers';
 
 export const open311Router = Router();
 
-function ifXml(path: string, res: Response) {
-    if (path.endsWith('xml')) {
-        res.status(501).send("501 Not Implemented");
-    }
+function isXML(path: string) {
+    if (path.endsWith('xml')) { return true } else { return false; }
 }
 
 open311Router.get('/', wrapAsync(async (req: Request, res: Response) => {
@@ -20,14 +20,20 @@ open311Router.get('/discovery', wrapAsync(async (req: Request, res: Response) =>
 
 open311Router.get(['/services.json', '/services.xml'], wrapAsync(async (req: Request, res: Response) => {
     // handle xml requests as we dont currently support
-    ifXml(req.path, res);
+    const asXML = isXML(req.path);
     const { Service } = res.app.repositories;
     const leafOnly = true;
     /* eslint-disable  prefer-const */
     let [records, count] = await Service.findAll("CLIENT_ID", leafOnly);
     /* eslint-enable prefer-const */
     records = records.map(serviceAs311);
-    res.status(200).send({ data: records, count: count });
+    if (asXML) {
+        const builder = new xml2js.Builder({ rootName: 'services' });
+        res.type('application/xml');
+        res.status(200).send(builder.buildObject({ service: records }));
+    } else {
+        res.status(200).send({ data: records, count: count });
+    }
 }))
 
 open311Router.get(['/services/:service_code.json', '/services/:service_code.xml'], wrapAsync(async (req: Request, res: Response) => {
@@ -42,9 +48,7 @@ open311Router.get(['/requests/:service_request_id.json', '/requests/:service_req
     res.status(501).send("501 Not Implemented");
 }))
 
-open311Router.post(['/requests.json', '/requests.xml'], wrapAsync(async (req: Request, res: Response) => {
-    // handle xml requests as we dont currently support
-    ifXml(req.path, res);
+open311Router.post(['/requests.json', '/requests.xml'], xmlparser({ trim: false, explicitArray: false }), wrapAsync(async (req: Request, res: Response) => {
     const { ServiceRequest } = res.app.repositories;
     const record = await ServiceRequest.create("CLIENT_ID", req.body);
     res.status(200).send({ data: record, success: true });
