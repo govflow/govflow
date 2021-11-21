dc12907f929fd01614a4270d9144da105da4d5f8
Paul Walsh <paul@walsh.co.il>
2021-11-21 18:21:19 +0200

    bump version.

--- src/core/index.ts
+++ src/core/index.ts
@@ -14,7 +14,7 @@ import { accountRouter, StaffUserModel, StaffUserRepository } from './staff-user
 const coreRoutes = Router();
 
 coreRoutes.get('/', wrapHandler(async (req: Request, res: Response) => {
-    res.status(200).send({ data: { name: 'govflow', version: '0.0.5-alpha' } });
+    res.status(200).send({ data: { name: 'govflow', version: '0.0.6-alpha' } });
 }))
 coreRoutes.use('/services', serviceRouter);
 coreRoutes.use('/service-requests', serviceRequestRouter);
