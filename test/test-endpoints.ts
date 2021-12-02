6947244f3bffc7043a64acd1892313a3fed3128e
Amir Ozer <amir@zencity.io>
2021-12-02 15:00:28 +0200

    0.0.14-alpha

--- test/test-endpoints.ts
+++ test/test-endpoints.ts
@@ -27,7 +27,7 @@ describe('Hit all API endpoints', function () {
         try {
             const res = await chai.request(app).get('/')
             chai.assert.equal(res.status, 200);
-            chai.assert.equal(res.text, JSON.stringify({ data: { name: 'govflow', version: '0.0.13-alpha' } }));
+            chai.assert.equal(res.text, JSON.stringify({ data: { name: 'govflow', version: '0.0.14-alpha' } }));
         } catch (error) {
             throw error;
         }
