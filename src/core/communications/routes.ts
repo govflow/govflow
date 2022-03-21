16b758e8e922cf61b1e19e61be1a47cb759a712b
Paul Walsh <paul@walsh.co.il>
2022-03-21 08:34:59 +0200

    Emit service request create for inbound emails.

--- src/core/communications/routes.ts
+++ src/core/communications/routes.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from 'express';
 import multer from 'multer';
 import { wrapHandler } from '../../helpers';
 import { enforceJurisdictionAccess, resolveJurisdiction } from '../../middlewares';
+import { GovFlowEmitter } from '../event-listeners';
 
 export const communicationsRouter = Router();
 
@@ -17,6 +18,8 @@ communicationsRouter.post('/create-map',
 
 communicationsRouter.post('/inbound/email', multer().none(), wrapHandler(async (req: Request, res: Response) => {
     const { InboundEmail } = res.app.repositories;
-    await InboundEmail.createServiceRequest(req.body);
+    const { Communication: dispatchHandler } = res.app.services;
+    const record = await InboundEmail.createServiceRequest(req.body);
+    GovFlowEmitter.emit('serviceRequestCreate', req.jurisdiction, record, dispatchHandler);
     res.status(200).send({ data: { status: 200, message: "Received inbound email" } });
 }))
