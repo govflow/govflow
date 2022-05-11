cdaac478c45aa22b5933c70371d40240c8528995
Paul Walsh <paul@walsh.co.il>
2022-05-11 14:52:41 +0300

    bump version

--- src/core/index.ts
+++ src/core/index.ts
@@ -16,7 +16,7 @@ import { storageRouter } from './storage';
 const coreRoutes = Router();
 
 coreRoutes.get('/', wrapHandler(async (req: Request, res: Response) => {
-    res.status(200).send({ data: { name: 'govflow', version: '0.0.73-alpha' } });
+    res.status(200).send({ data: { name: 'govflow', version: '0.0.74-alpha' } });
 }))
 coreRoutes.use('/services', serviceRouter);
 coreRoutes.use('/service-requests', serviceRequestRouter);
